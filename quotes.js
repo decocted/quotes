c38c491a45e5b2990ded988aa076daa145112f82
George <g3eorge@pm.me>
2025-07-22 01:32:19 -0400

    Update quotes.js

--- quotes.js
+++ quotes.js
@@ -2,5 +2,9 @@ const quotes = [
   {
     "text": "Keep your face always toward the sunshine and shadows will fall behind you",
     "author": "Walt Whitman"
+  },
+  {
+    "text": "You cannot escape the responsiblity of tomorrow by evading it today",
+    "author": "Abraham Lincoln"
   }
 ]
